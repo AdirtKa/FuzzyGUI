a23801c463e9b4231a203599e3dd46b34f67a6a5
adirtka <AdirtKa@yandex.ru>
2025-10-20 10:22:53 +1000

    add real data

--- fuzzy_system.py
+++ fuzzy_system.py
@@ -12,16 +12,16 @@ def build_system(defuzzify_method="centroid"):
     tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip', defuzzify_method=defuzzify_method)
 
     # --- Функции принадлежности ---
-    food['bad'] = fuzz.trimf(food.universe, [0, 0, 5])
-    food['good'] = fuzz.trimf(food.universe, [5, 10, 10])
+    food['bad'] = fuzz.trapmf(food.universe, [0, 0, 4, 6])
+    food['good'] = fuzz.trapmf(food.universe, [5, 7, 10, 10])
 
-    service['poor'] = fuzz.trimf(service.universe, [0, 0, 5])
-    service['average'] = fuzz.trimf(service.universe, [2, 5, 8])
-    service['excellent'] = fuzz.trimf(service.universe, [5, 10, 10])
+    service['poor'] = fuzz.trapmf(service.universe, [0, 0, 2, 4])
+    service['average'] = fuzz.trimf(service.universe, [3, 5, 7])
+    service['excellent'] = fuzz.trapmf(service.universe, [6, 8, 10, 10])
 
-    tip['low'] = fuzz.trapmf(tip.universe, [0, 0, 5, 10])
-    tip['medium'] = fuzz.trimf(tip.universe, [5, 12, 17])
-    tip['high'] = fuzz.trapmf(tip.universe, [15, 20, 25, 25])
+    tip['low'] = fuzz.trapmf(tip.universe, [0, 0, 4, 7])
+    tip['medium'] = fuzz.trimf(tip.universe, [8, 12, 16])
+    tip['high'] = fuzz.trapmf(tip.universe, [8, 21, 25, 25])
 
     # --- Правила ---
     rule1 = ctrl.Rule(service['poor'] | food['bad'], tip['low'])
